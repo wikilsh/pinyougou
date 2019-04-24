0bc6ccf8078ab533f63c6804d68ea5e512d81e4c
wiki_hui <1365236295@qq.com>
2019-04-24 11:22:26 +0800

    规格和模板管理开发基本完成

--- pinyougou-manager-web/src/main/webapp/js/controller/typeTemplateController.js
+++ pinyougou-manager-web/src/main/webapp/js/controller/typeTemplateController.js
@@ -26,7 +26,11 @@ app.controller('typeTemplateController' ,function($scope,$controller	,typeTempla
 	$scope.findOne=function(id){				
 		typeTemplateService.findOne(id).success(
 			function(response){
-				$scope.entity= response;					
+				$scope.entity= response;	
+				
+				$scope.entity.brandIds=  JSON.parse($scope.entity.brandIds);//转换品牌列表
+				$scope.entity.specIds=  JSON.parse($scope.entity.specIds);//转换规格列表
+				$scope.entity.customAttributeItems= JSON.parse($scope.entity.customAttributeItems);//转换扩展属性 
 			}
 		);				
 	}
@@ -95,6 +99,14 @@ app.controller('typeTemplateController' ,function($scope,$controller	,typeTempla
 				$scope.specList={data:response};
 			}	
 		);
+	}
+	//新增扩展属性行
+	$scope.addTableRow=function(){
+		$scope.entity.customAttributeItems.push({});
+	}
+	//删除扩展属性行
+	$scope.deleTableRow=function(index){
+		$scope.entity.customAttributeItems.splice(index,1);//删除
 	}
 	
 });	
